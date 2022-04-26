c43fdb9ee9be12f80829755608681e1b93ac2b18
didch1789 <didch1789@gmail.com>
2022-04-26 00:31:07 +0000

    updates

--- myfunc/measure_orths.m
+++ myfunc/measure_orths.m
@@ -1,4 +1,16 @@
-function out = measure_orths(mat1, mat2, dorand)
+function out = measure_orths(mat1, mat2, varargin)
+
+dorand = false;
+for i = 1:numel(varargin)
+    if ischar(varargin{i})
+        switch varargin{i}
+            case {'dorand'}
+                dorand = true;
+            case {'numcomps', 'numcomp'}
+                numPCs = varargin{i+1};
+        end
+    end
+end
 
 corrmat1 = reformat_r_new(corr(mat1), 'flatten');
 corrmat2 = reformat_r_new(corr(mat2), 'flatten');
@@ -11,12 +23,14 @@ out.Pair_Wise_Corr.corr =  corr12;
 out.Pair_Wise_Corr.rawcorrs = [corrmat1, corrmat2];
 out.Pair_Wise_Corr.cross_condition_corr = cross_cond_corr;
 if dorand 
-    out.Pair_Wise_Corr.randomidx = bootci(5000, @nancorr, corrmat1, corrmat2);
+    out.Pair_Wise_Corr.randomidx = bootci(1000, @nancorr, corrmat1, corrmat2);
 end
 
-[~, ~, ~, ~, expl] = pca([mat1;mat2]);
-% numPCs = min(find(cumsum(expl) > 90));
-numPCs = 5;
+if ~exist('numPCs', 'var')
+    [~, ~, ~, ~, expl] = pca([mat1;mat2]);
+    % numPCs = min(find(cumsum(expl) > 90));
+    numPCs = 10;
+end
 mat1PCs   = pca(mat1, 'NumComponents', numPCs);
 mat2PCs   = pca(mat2, 'NumComponents', numPCs);
 
@@ -27,10 +41,10 @@ if dorand
 end
                               
 out.Explained.mat1_by_mat1 =  (var((mat1 - mean(mat1)) * mat1PCs) ./ sum(var(mat1)))';
-out.Explained.mat2_by_mat1 =  (var((mat2 - mean(mat2)) * mat1PCs) ./ sum(var(mat2)))';
+out.Explained.mat1_by_mat2 = (var((mat1 - mean(mat1)) * mat2PCs) ./ sum(var(mat1)))';
 
 out.Explained.mat2_by_mat2 = (var((mat2 - mean(mat2)) * mat2PCs) ./ sum(var(mat2)))';
-out.Explained.mat1_by_mat2 = (var((mat1 - mean(mat1)) * mat2PCs) ./ sum(var(mat1)))';
+out.Explained.mat2_by_mat1 =  (var((mat2 - mean(mat2)) * mat1PCs) ./ sum(var(mat2)))';
 
 out.numPCs = numPCs;
 
